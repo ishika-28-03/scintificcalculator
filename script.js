494ba362e0340f31d57eb6e375a863de3c1b2148
ishika prasad <dp878917@gmail.com>
2023-08-14 15:24:38 +0530

    COMMIT

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ arr.forEach(button =>{
             string=string.substring(0,string.length-1);
             input.value=string;
         }   
+        else if(e.target.innerHTML=="Sq"){
+            string=123;
+            input.value=string;
+        }
         else{
             string+=e.target.innerHTML;
             input.value=string;
